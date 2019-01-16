156e234f4d1307342747d1f95ef112e6bfe4c2d6
Your Name <you@example.com>
2019-01-16 15:55:57 +0000

    fixes in statistical analysis code

--- src/analysis/stats.py
+++ src/analysis/stats.py
@@ -2,6 +2,8 @@ import json
 import pickle
 from collections import defaultdict
 import os
+import numpy as np
+import pickle
 
 # # read the mesh to index files
 # with open('../data/mesh_to_idx.pkl', 'rb') as fread:
@@ -29,17 +31,28 @@ for fi in files:
 
 n_labels = sum(list(label_cnt.values()))
 
+print ("Total # of labels: ", n_labels, "Max # of any one label: ", np.max(list(label_cnt.values())))
+
 for label in label_cnt.keys():
 	label_cnt[label] = label_cnt[label]/float(n_labels)
 
-
-thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+thresholds = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
 cnts = np.array(list(label_cnt.values()))
 
 for threshold in thresholds:
 	n_labels_threshold = np.sum(cnts>threshold)
 	print ("# of labels with frequency above ", threshold, " are: ", n_labels_threshold)
 
+pickle.dump(label_cnt, open('../data/label_cnt.pkl','wb'))
+
+label_cnt_fractions = sorted(np.array(list(label_cnt.values())), reverse=True)
+label_cnt_cumsum = np.cumsum(label_cnt_fractions)
+
+pickle.dump(label_cnt_cumsum, open('../data/label_cnt_cumsum.pkl','wb'))
+
+print ("Cumulative sum of label counts:")
+for i in label_cnt_cumsum:
+	print (i)
 
 
 
